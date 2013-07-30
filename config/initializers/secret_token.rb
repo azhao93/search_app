71096deb1073d0c431545e26077845b415a372e0
Aojia Zhao <aojia93@stanford.edu>
2013-07-30 15:55:30 +0900

    Creating new Repos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SearchApp::Application.config.secret_key_base = 'c999fde0d55e0d4ea7f216712a6ad036cc3cfacf49ed9f702ace5ad55bb229760e145dea921591535ba4ab8a469a3b6fc9bffb5270f198addbeed0a6cc3b10ae'
